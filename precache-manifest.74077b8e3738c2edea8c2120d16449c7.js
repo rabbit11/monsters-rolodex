df25ac14cc65c6ba63500868cc727c4309c191fa
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-10-01 20:53:50 -0300

    Updates

---
+++ precache-manifest.74077b8e3738c2edea8c2120d16449c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433e6f7fc10c9be14221c4785b1e2738",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b28464d6af3169550e23",
+    "url": "/monsters-rolodex/static/css/main.f4beedb9.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "b28464d6af3169550e23",
+    "url": "/monsters-rolodex/static/js/main.b07b8200.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
